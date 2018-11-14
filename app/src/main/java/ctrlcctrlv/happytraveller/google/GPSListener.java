4509ab779675090b223ac51fc80028b2e0225bb7
LaMDom <aimilianoskas@gmail.com>
2018-11-14 10:58:29 +0200

    fixing and suggesting
    Delete unnecessary code and made some todo for xatzako

--- app/src/main/java/ctrlcctrlv/happytraveller/google/GPSListener.java
+++ app/src/main/java/ctrlcctrlv/happytraveller/google/GPSListener.java
@@ -9,43 +9,58 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
-public class GPSListener extends Service {
+// TODO: 11/14/2018 den nomizw pws auth h klash tha prepei na briskete sto package google
+
+// TODO: 11/14/2018 {TSEKARE AUTO ->}  Activities, services and content providers should be registered in the AndroidManifest.xml file using <activity>, <service> and <provider> tags.  If your activity is simply a parent class intended to be subclassed by other "real" activities, make it an abstract class.Issue id: Registered  More info: http://developer.android.com/guide/topics/manifest/manifest-intro.html
+//               |
+//               |
+//              \ /
+//               v
+public class GPSListener extends Service
+{
     private static final String TAG = "BOOMBOOMTESTGPS";
     private LocationManager mLocationManager = null;
     private static final int LOCATION_INTERVAL = 5000;
     private static final float LOCATION_DISTANCE = 10f;
 
-    private class LocationListener implements android.location.LocationListener {
+    private class LocationListener implements android.location.LocationListener
+    {
         Location mLastLocation;
 
-        public LocationListener(String provider) {
+        public LocationListener(String provider)
+        {
             Log.e(TAG, "LocationListener " + provider);
             mLastLocation = new Location(provider);
         }
 
         @Override
-        public void onLocationChanged(Location location) {
+        public void onLocationChanged(Location location)
+        {
             Log.e(TAG, "onLocationChanged: " + location);
             mLastLocation.set(location);
         }
 
         @Override
-        public void onProviderDisabled(String provider) {
+        public void onProviderDisabled(String provider)
+        {
             Log.e(TAG, "onProviderDisabled: " + provider);
         }
 
         @Override
-        public void onProviderEnabled(String provider) {
+        public void onProviderEnabled(String provider)
+        {
             Log.e(TAG, "onProviderEnabled: " + provider);
         }
 
         @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
+        public void onStatusChanged(String provider, int status, Bundle extras)
+        {
             Log.e(TAG, "onStatusChanged: " + provider);
         }
     }
 
-    LocationListener[] mLocationListeners = new LocationListener[]{
+    LocationListener[] mLocationListeners = new LocationListener[]
+            {
             new LocationListener(LocationManager.GPS_PROVIDER),
             new LocationListener(LocationManager.NETWORK_PROVIDER)
     };
@@ -56,14 +71,16 @@ public class GPSListener extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
         Log.e(TAG, "onStartCommand");
         super.onStartCommand(intent, flags, startId);
         return START_STICKY;
     }
 
     @Override
-    public void onCreate() {
+    public void onCreate()
+    {
         Log.e(TAG, "onCreate");
         initializeLocationManager();
         try {
@@ -87,7 +104,8 @@ public class GPSListener extends Service {
     }
 
     @Override
-    public void onDestroy() {
+    public void onDestroy()
+    {
         Log.e(TAG, "onDestroy");
         super.onDestroy();
         if (mLocationManager != null) {
@@ -95,15 +113,17 @@ public class GPSListener extends Service {
                 try {
                     mLocationManager.removeUpdates(mLocationListeners[i]);
                 } catch (Exception ex) {
-                    Log.i(TAG, "fail to remove location listners, ignore", ex);
+                    Log.i(TAG, "fail to remove location listeners, ignore", ex);
                 }
             }
         }
     }
 
-    private void initializeLocationManager() {
+    private void initializeLocationManager()
+    {
         Log.e(TAG, "initializeLocationManager");
-        if (mLocationManager == null) {
+        if (mLocationManager == null)
+        {
             mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
         }
     }
