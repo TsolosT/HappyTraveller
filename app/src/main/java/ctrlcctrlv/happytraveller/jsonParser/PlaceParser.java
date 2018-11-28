ea96aaf0c13397885ca3959da81bf2b3a7f7023d
Themis Zouboulakis <30598253+IanMoonee@users.noreply.github.com>
2018-11-28 16:12:13 +0200

    get,set functions for lat,long

--- app/src/main/java/ctrlcctrlv/happytraveller/jsonParser/PlaceParser.java
+++ app/src/main/java/ctrlcctrlv/happytraveller/jsonParser/PlaceParser.java
@@ -14,19 +14,30 @@ public class PlaceParser {
     public static ArrayList parseGoogleParse(final String response) {
 
         ArrayList temp = new ArrayList();
+        JSONObject geometryObject;
+        JSONObject locationObject;
+        JSONObject JsonObj;
+        Double latitude=null;
+        Double longitude=null;
         try {
             // make an jsonObject in order to parse the response
             JSONObject jsonObject = new JSONObject(response);
 
 
             // make an jsonObject in order to parse the response
-            if (jsonObject.has("results")) {
+            if (jsonObject.has("results"))
+            {
 
                 JSONArray jsonArray = jsonObject.getJSONArray("results");
                 ArrayList<PlacePhoto> placePhotos=new ArrayList<>();
 
                 for (int i = 0; i < jsonArray.length(); i++)
                 {
+                    JsonObj = jsonArray.getJSONObject(i);
+                    geometryObject = JsonObj.getJSONObject("geometry");
+                    locationObject = geometryObject.getJSONObject("location");
+                    latitude = locationObject.getDouble("lat");
+                    longitude = locationObject.getDouble("lng");
                         //ka8e new name = new json obj
                     if (jsonArray.getJSONObject(i).has("name"))
                     {   //at the moment get only one img for each place
@@ -38,11 +49,11 @@ public class PlaceParser {
 //                            {
 //                                placePhotos.add(new PlacePhoto(((JSONObject) photos.get(j)).getString("photo_reference")));
 //                            }
-                            temp.add(new PlaceData(jsonArray.getJSONObject(i).optString("name"), jsonArray.getJSONObject(i).optString("vicinity"),new PlacePhoto(((JSONObject) photos.get(0)).getString("photo_reference"))));
+                            temp.add(new PlaceData(jsonArray.getJSONObject(i).optString("name"), jsonArray.getJSONObject(i).optString("vicinity"),new PlacePhoto(((JSONObject) photos.get(0)).getString("photo_reference")),latitude,longitude));
                         }
                         else
                         {
-                            temp.add(new PlaceData(jsonArray.getJSONObject(i).optString("name"), jsonArray.getJSONObject(i).optString("vicinity"),new PlacePhoto(null)));
+                            temp.add(new PlaceData(jsonArray.getJSONObject(i).optString("name"), jsonArray.getJSONObject(i).optString("vicinity"),new PlacePhoto(null),latitude,longitude));
                         }
 
                     }
