70aa3fda61ef8cadceaf9ab462a127978979841f
xaroula <44241068+xaroula@users.noreply.github.com>
2018-12-08 14:50:31 +0200

    info_Button
    prostethike kwdikas gia to koumpi plirofories

--- app/src/main/java/ctrlcctrlv/happytraveller/activities/InfoActivity.java
+++ app/src/main/java/ctrlcctrlv/happytraveller/activities/InfoActivity.java
@@ -2,6 +2,7 @@ package ctrlcctrlv.happytraveller.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import ctrlcctrlv.happytraveller.R;
 
@@ -12,5 +13,9 @@ public class InfoActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-    }
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl("file:///android_asset/InfoGuide.html");
+
+     }
 }
