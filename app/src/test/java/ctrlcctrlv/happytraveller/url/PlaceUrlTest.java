7fbf6b8141012860048c59f3c96e5f351abf1191
ggiannas <g.giannas@yahoo.gr>
2018-12-11 14:32:01 +0200

    add some testing on PlaceUrl

--- app/src/test/java/ctrlcctrlv/happytraveller/url/PlaceUrlTest.java
+++ app/src/test/java/ctrlcctrlv/happytraveller/url/PlaceUrlTest.java
@@ -10,12 +10,32 @@ import static org.junit.Assert.*;
 public class PlaceUrlTest {
 
     PlaceUrl urlTest;
+    PlaceUrl urlTestAll;
 
     @Before
     public void setUp(){
         urlTest=new PlaceUrl();
         urlTest.setLatLng("41.0943488,23.5544576");
         urlTest.setPlaceType("museum");
+    }
+
+    @Before
+    public void newSetUp()
+    {
+        urlTestAll=new PlaceUrl();
+        urlTestAll.setLatLng("41.082100,23.549980");
+    }
+    @Test
+    public void getUrlWithThousandRadiusCorrect() {
+
+        String urlExpected="https://maps.googleapis.com/maps/api/place/search/json?location=41.0943488,23.5544576&type=museum&radius=10000&sensor=true&key=AIzaSyDI0zKd22JBJEGco2k9Thg2CZWLLsWfq7k";
+        assertEquals(urlExpected,urlTestAll.getUrl());
+    }
+    @Test
+    public void getUrlWithThousandRadiusWrong() {
+
+        String urlExpected="https://maps.googleapis.com/maps/api/place/search/json?location=41.0943488,23.5544576&type=museum&radius=10000&sensor=true&key=AIzaSyDI0zKd22JBJEGco2k9Thg2CZWLLsWfq7k";
+        assertNotEquals(urlExpected,urlTestAll.getUrl());
     }
 
     @Test
@@ -57,10 +77,81 @@ public class PlaceUrlTest {
     }
 
     @Test
+    public void getPlaceTypeMuseumCorrect() {
+        String expected="museum";
+        assertEquals(expected,urlTestAll.getPlaceType());
+    }
+    @Test
+    public void getPlaceTypeMuseumWrong() {
+        String expected="museum";
+        urlTest.setPlaceType("park");
+        assertNotEquals(expected,urlTestAll.getPlaceType());
+    }
+
+
+    @Test
+    public void getPlaceTypeParkCorrect() {
+        String expected="park";
+        assertEquals(expected,urlTestAll.getPlaceType());
+    }
+    @Test
+    public void getPlaceTypeParkWrong() {
+        String expected="park";
+        urlTestAll.setPlaceType("museum");
+        assertNotEquals(expected,urlTestAll.getPlaceType());
+    }
+
+    @Test
+    public void getPlaceTypeChurchCorrect() {
+        String expected="church";
+        assertEquals(expected,urlTest.getPlaceType());
+    }
+    @Test
+    public void getPlaceTypeChurchWrong() {
+        String expected="church";
+        urlTestAll.setPlaceType("park");
+        assertNotEquals(expected,urlTestAll.getPlaceType());
+    }
+
+
+    @Test
+    public void setPlaceTypeMuseumCorrect() {
+        String expected="museum";
+        assertEquals(expected,urlTestAll.getPlaceType());
+    }
+    @Test
+    public void setPlaceTypeMuseumWrong() {
+        String expected="museum";
+        assertNotEquals(expected,urlTestAll.getPlaceType());
+    }
+    @Test
+    public void setPlaceTypeParkCorrect() {
+        String expected="park";
+        assertEquals(expected,urlTestAll.getPlaceType());
+    }
+    @Test
+    public void setPlaceTypeParkWrong() {
+        String expected="park";
+        assertNotEquals(expected,urlTestAll.getPlaceType());
+    }
+    @Test
+    public void setPlaceTypeChurchCorrect() {
+        String expected="church";
+        assertEquals(expected,urlTestAll.getPlaceType());
+    }
+    @Test
+    public void setPlaceTypeChurchWrong() {
+        String expected="church";
+        assertNotEquals(expected,urlTestAll.getPlaceType());
+    }
+
+
+    @Test
     public void getLatLngCorrect() {
         String expected="41.0943488,23.5544576";
         assertEquals(expected,urlTest.getLatLng());
     }
+
     @Test
     public void getLatLngWrong() {
         String expected="41.0943488,23.5544576";
@@ -78,9 +169,34 @@ public class PlaceUrlTest {
         urlTest.setLatLng("22.21123,11.232323");
         assertNotEquals("22.22323,11.232323",urlTest.getLatLng());
     }
+
+    @Test
+    public void getMyLatLngCorrect() {
+        String expected="41.082100,23.549980";
+        assertEquals(expected,urlTestAll.getLatLng());
+    }
+    @Test
+    public void getMyLatLngWrong() {
+        String expected="41.082100,23.549980";
+        urlTest.setLatLng(null);
+        assertNotEquals(expected,urlTestAll.getLatLng());
+    }
+
+    @Test
+    public void setMyLatLngCorrect() {
+        urlTest.setLatLng("25.24323,51.234423");
+        assertEquals("25.22323,51.232323",urlTestAll.getLatLng());
+    }
+    @Test
+    public void setMyLatLngWrong() {
+        urlTest.setLatLng("25.21123,51.232323");
+        assertNotEquals("25.22323,51.232323",urlTestAll.getLatLng());
+    }
     @After
     public void clearObject()
     {
         urlTest=null;
     }
+    @After
+    public void clearObjectAll (){urlTestAll=null;}
 }
