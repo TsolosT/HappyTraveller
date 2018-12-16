42be275ec72c7b9376fdf36e6148c094f355b927
Xatzakos <kxatzakos@gmail.com>
2018-12-16 18:48:04 +0200

    Improved search url for detailsactivity
    123

--- app/src/main/java/ctrlcctrlv/happytraveller/activities/DetailsActivity.java
+++ app/src/main/java/ctrlcctrlv/happytraveller/activities/DetailsActivity.java
@@ -1,6 +1,5 @@
 package ctrlcctrlv.happytraveller.activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +8,8 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import ctrlcctrlv.happytraveller.R;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity
+{
 
     //ui
     public WebView wiki;
@@ -17,8 +17,7 @@ public class DetailsActivity extends AppCompatActivity {
     private static String searchName = null;
     static String URLsearch = "";
 
-    public DetailsActivity(Context mMockContext) {
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +41,7 @@ public class DetailsActivity extends AppCompatActivity {
 
 
         //Redirects to first google search result from " <name> wikipedia " search
-        URLsearch="https://www.google.com/search?q={"+searchName+" wikipedia}&btnI";
+        URLsearch="https://www.google.com/search?q={"+searchName+" σερρες wikipedia}&btnI";
 
         //fetch the first result
         wiki.loadUrl(URLsearch);
